25efe0f2c60f6dc523f72e84263075f805efa98d
TheEmpressDiadema <mauhkpaffftep@gmail.com>
2026-09-12 20:40:46 +0300

    refactor: accepted fuff formatting

--- src/currency_exchange/server/router.py
+++ src/currency_exchange/server/router.py
@@ -1,5 +1,5 @@
-from dataclasses import dataclass
 from collections.abc import Callable
+from dataclasses import dataclass
 
 from currency_exchange.server.structures import HttpRequest, HttpResponse
 

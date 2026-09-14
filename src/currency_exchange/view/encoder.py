dc19affd96776b5b0701cf74b9e7aedba7b56a14
TheEmpressDiadema <mauhkpaffftep@gmail.com>
2026-09-14 19:23:02 +0300

    refactor: deleted useless code, renamed dto with dict_object

--- src/currency_exchange/view/encoder.py
+++ src/currency_exchange/view/encoder.py
@@ -8,8 +8,5 @@ def create_list_view(dtos: list[dict[str, Any]]) -> str:
         result.append(json.loads(create_view(dto)))
     return json.dumps(result, indent=4, ensure_ascii=False)
 
-def create_view(dto_dict: dict[str, Any]) -> str:
-    result = {}
-    for key, value in dto_dict.items():
-        result[key] = value
-    return json.dumps(result, indent=4, ensure_ascii=False)
+def create_view(dict_object: dict[str, Any]) -> str:
+    return json.dumps(dict_object, indent=4, ensure_ascii=False)
